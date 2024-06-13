5810884df9c4327fb5719b4a8d6cdbe3478bebb1
Deepika-Partha <88789540+Deepika-Partha@users.noreply.github.com>
2024-06-13 13:02:02 -0400

    Create StudentGradesBook.java

---
+++ StudentGradesBook.java
@@ -0,0 +1,37 @@
+/*
+* Names: Deepika Parthasarathy
+* netID: dparthas
+* G#: 01365740
+* Lecture section: 
+* Lab section:
+*/
+
+public class StudentGradesBook {
+
+    private double participationWeight;
+    private double readingsWeight;
+    private double labsWeight;
+    private double exercisesWeight;
+    private double projectsWeight;
+    private double midtermWeight;
+    private double finalExamWeight;
+
+    public String toString() {
+        String rv = "Name: " + getStudentName() + "\n";
+        rv += "G#: " + getGNumber() + "\n";
+        rv += "Participation: " + getParticipation() + "\n";
+        rv += "Readings: " + unweightedReadingsScore() + "\n";
+        rv += "Labs: " + unweightedLabsScore() + "\n";
+        rv += "Exercises: " + unweightedExercisesScore() + "\n";
+        rv += "Projects: " + unweightedProjectsScore() + "\n";
+        rv += "Midterm: " + getMidterm() + "\n";
+        rv += "Final Exam: " + getFinalExam() + "\n";
+        rv += "Total Score: " + totalScore() + "\n";
+        rv += "Letter Grade: " + letterGrade() + "\n";
+        return rv;
+    }
+    
+    public static void main(String[]args){
+    
+    }
+}
